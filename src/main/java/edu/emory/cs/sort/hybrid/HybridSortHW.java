b36796a877b1672d70b9eeaa789226319f6ffef6
Aayam <akc6@emory.edu>
2023-02-26 19:19:08 -0500

    it's a little faster

--- src/main/java/edu/emory/cs/sort/hybrid/HybridSortHW.java
+++ src/main/java/edu/emory/cs/sort/hybrid/HybridSortHW.java
@@ -1,18 +1,19 @@
 package edu.emory.cs.sort.hybrid;
 
 import edu.emory.cs.sort.AbstractSort;
+import edu.emory.cs.sort.comparison.HeapSort;
 import edu.emory.cs.sort.comparison.InsertionSort;
 import edu.emory.cs.sort.comparison.ShellSortKnuth;
+import edu.emory.cs.sort.comparison.ShellSortQuiz;
 import edu.emory.cs.sort.divide_conquer.IntroSort;
-
-
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+
 public class HybridSortHW<T extends Comparable<T>> implements HybridSort<T> {
-    private static final int DEFAULT_PARALLELISM = Runtime.getRuntime().availableProcessors();
+    private static final int DEFAULT_PARALLELISM = 8;
     private final AbstractSort<T> engine;
 
     public HybridSortHW() {
@@ -26,10 +27,8 @@ public class HybridSortHW<T extends Comparable<T>> implements HybridSort<T> {
     }
 
     public void sortRows(T[][] arr) {
-        int threshold = 10;
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            T[] row = arr[i];
+        int threshold = 15;
+        for (T[] row : arr) {
             if (row.length > threshold) {
                 engine.sort(row);
             } else {
@@ -38,40 +37,90 @@ public class HybridSortHW<T extends Comparable<T>> implements HybridSort<T> {
         }
     }
 
-    @SuppressWarnings("unchecked")
     public T[] mergeRows(T[][] input) {
         int numRows = input.length;
         int totalSize = Arrays.stream(input).mapToInt(row -> row.length).sum();
 
-        // Create a priority queue to hold the values from each row
-        PriorityQueue<IndexedValue<T>> queue = new PriorityQueue<>(numRows,
-                Comparator.comparing(IndexedValue::getValue));
+        // Create an array to hold the values from each row
+        T[] output = (T[]) Array.newInstance(input[0][0].getClass(), totalSize);
 
-        // Add the first value from each row to the queue
+        // Create a merge heap to hold the first value from each row
+        MergeHeap<T> heap = new MergeHeap<>(numRows);
         for (int i = 0; i < numRows; i++) {
             T[] row = input[i];
             if (row.length > 0) {
-                queue.add(new IndexedValue<>(row[0], i, 0));
+                heap.add(new IndexedValue<>(row[0], i, 0));
             }
         }
 
-        // Merge the rows by repeatedly removing the smallest value from the queue
-        T[] output = (T[]) Array.newInstance(input[0][0].getClass(), totalSize);
+        // Merge the rows by repeatedly removing the smallest value from the merge heap
         int outputIndex = 0;
-        while (!queue.isEmpty()) {
-            IndexedValue<T> value = queue.poll();
+        while (!heap.isEmpty()) {
+            IndexedValue<T> value = heap.removeMin();
             output[outputIndex++] = value.getValue();
 
-            // If there are more values in the same row, add the next value to the queue
+            // If there are more values in the same row, add the next value to the merge heap
             int colIndex = value.getColIndex() + 1;
             if (colIndex < input[value.getRowIndex()].length) {
-                queue.add(new IndexedValue<>(input[value.getRowIndex()][colIndex],
+                heap.add(new IndexedValue<>(input[value.getRowIndex()][colIndex],
                         value.getRowIndex(), colIndex));
             }
         }
 
         return output;
     }
+
+    // A merge heap implementation
+    private static class MergeHeap<T extends Comparable<T>> {
+        private final IndexedValue<T>[] heap;
+        private int size;
+
+        @SuppressWarnings("unchecked")
+        public MergeHeap(int capacity) {
+            heap = (IndexedValue<T>[]) Array.newInstance(IndexedValue.class, capacity + 1);
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public void add(IndexedValue<T> value) {
+            heap[++size] = value;
+            swim(size);
+        }
+
+        public IndexedValue<T> removeMin() {
+            IndexedValue<T> min = heap[1];
+            heap[1] = heap[size--];
+            sink(1);
+            return min;
+        }
+
+        private void swim(int i) {
+            while (i > 1 && heap[i].compareTo(heap[i/2]) < 0) {
+                swap(i, i/2);
+                i /= 2;
+            }
+        }
+
+        private void sink(int i) {
+            while (2*i <= size) {
+                int j = 2*i;
+                if (j < size && heap[j+1].compareTo(heap[j]) < 0) j++;
+                if (heap[j].compareTo(heap[i]) >= 0) break;
+                swap(i, j);
+                i = j;
+            }
+        }
+
+        private void swap(int i, int j) {
+            IndexedValue<T> temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
+        }
+    }
+
 
     // A helper class to hold a value along with its row and column indices
     private static class IndexedValue<T extends Comparable<T>> implements Comparable<IndexedValue<T>> {
